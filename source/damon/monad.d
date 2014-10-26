59daa07d34302ef50fc5c76504cd4c55fc5c184e
Snaipe <franklinmathieu@gmail.com>
2014-10-26 20:12:27 +0100

    Added Many monad

--- source/damon/monad.d
+++ source/damon/monad.d
@@ -1,6 +1,7 @@
 module damon.monad;
 
 import std.traits;
+import damon.functional;
 
 template isLesserMonadicOperation(M, alias T, F) {
 	alias Monad = TemplateOf!M;
@@ -52,6 +53,12 @@ template from_value(alias M) {
 	}
 }
 
+template from_values(alias M) {
+	M!V from_values(V)(V[] val) {
+		return new M!V(val);
+	}
+}
+
 class Maybe(T) : Monad!(T) {
 	private T val;
 
@@ -93,4 +100,61 @@ unittest {
 	assert (a1.bind(getB).bind(getC).value is c);
 	assert ((a1 >> getB >> getC).value is c);
 	assert (a2.b.c.value is null);
+}
+
+class Many(T) : Monad!(T) {
+	private T[] val;
+
+	this(T[] val) { this.val = val; }
+
+	@property T[] values() { return this.val; }
+	@property ulong length() { return this.val.length; }
+
+	private Many!R flatten(R)(Many!R[] array) {
+		size_t len = 0;
+		foreach (r; array)
+			len += r.length;
+		R[] result = new R[len];
+
+		size_t i = 0;
+		foreach (t; array)
+			foreach (e; t.values)
+				result[i++] = e;
+
+		return new Many!R(result);
+	}
+
+	ReturnType!F bind(F)(F callback) if (isMonadicOperation!(Many, T, F)) {
+		return flatten(fmap(val, callback));
+	}
+
+	T opIndex(size_t i) {
+		return val[i];
+	}
+
+	auto opDispatch(string s)() {
+		return bind((T v) => from_values!Many(__traits(getMember, v, s)));
+	}
+
+	mixin monadicOperations!(Many);
+}
+
+unittest {
+	class B {
+		int[] arr;
+		this(int[] arr) { this.arr = arr; }
+	}
+	class A {
+		B[] bs;
+		this(B[] arr) { this.bs = arr; }
+	}
+
+	Many!A a = new Many!A([
+			new A([new B([1, 2]), new B([3, 4])]),
+			new A([new B([5, 6, 7, 8])])
+	]);
+
+	int[] result = a.bs.arr.values;
+	for (int i = 0; i < result.length; ++i)
+		assert (result[i] == i + 1);
 }
