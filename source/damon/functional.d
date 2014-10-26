de397a66cfa64604ebd59457a03bda6d92fd31da
Snaipe <franklinmathieu@gmail.com>
2014-10-26 20:03:19 +0100

    Added function currying

--- source/damon/functional.d
+++ source/damon/functional.d
@@ -41,3 +41,55 @@ class Function(alias F) if (isSomeFunction!F) {
 
 	mixin functionOperations;
 }
+
+template curry(alias F) {
+	class CurryiedFunction(int provided) {
+		private ParameterTypeTuple!F args;
+
+		this(ParameterTypeTuple!F args) { this.args = args; }
+
+		auto opCall(T...)(T params) {
+			static assert (provided + T.length <= ParameterTypeTuple!F.length,
+					"Curryied function cannot be called with more"
+					"arguments than its original definition");
+
+			// This is a temporary hack to overcome the lack of static
+			// iteration for args & params...
+			static assert (T.length <= 4,
+					"Partial function application does not yet support"
+					"more than 4 parameters at a time.");
+
+			static if (provided     < args.length && T.length >= 1)
+				args[provided] = params[0];
+			static if (provided + 1 < args.length && T.length >= 2)
+				args[provided + 1] = params[1];
+			static if (provided + 2 < args.length && T.length >= 3)
+				args[provided + 2] = params[2];
+			static if (provided + 3 < args.length && T.length >= 4)
+				args[provided + 3] = params[3];
+
+
+			static if (provided + T.length == ParameterTypeTuple!F.length)
+				return F(args);
+			else
+				return new CurryiedFunction!(provided + T.length)(args);
+		}
+
+		mixin functionOperations;
+	}
+	auto curry() {
+		ParameterTypeTuple!F empty;
+		return new CurryiedFunction!0(empty);
+	}
+}
+
+unittest {
+	long test(int i, long j) {
+		return i * i + j;
+	}
+	auto partial = curry!test;
+	assert (partial(7)(2) == test(7, 2));
+	assert (partial(7, 2) == test(7, 2));
+	assert (partial()(7)()(2) == test(7, 2));
+	assert (!__traits(compiles, partial(7, 2, 3)));
+}
