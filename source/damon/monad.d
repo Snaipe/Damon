491fa031e2fd0fdabf127e962958d82c229fde97
Snaipe <franklinmathieu@gmail.com>
2014-10-26 20:09:49 +0100

    Added Maybe monad

--- source/damon/monad.d
+++ source/damon/monad.d
@@ -35,3 +35,62 @@ mixin template monadicOperations(M) {
 		return bindF(rhs);
 	}
 }
+
+mixin template dispatch(alias F, alias M) {
+	auto opDispatch(string s)() {
+		return bind((T v) => F!M(__traits(getMember, v, s)));
+	}
+
+	auto opDispatch(string s, Args...)(Args args) {
+		return bind((T v) => F!M(__traits(getMember, v, s)(args)));
+	}
+}
+
+template from_value(alias M) {
+	M!V from_value(V)(V val) {
+		return new M!V(val);
+	}
+}
+
+class Maybe(T) : Monad!(T) {
+	private T val;
+
+	this(T val) { this.val = val; }
+	this()      { this(null); }
+
+	@property T value() { return this.val; }
+
+	ReturnType!F bind(F)(F callback) if (isMonadicOperation!(Maybe, T, F)) {
+		if (this.val is null)
+			return new ReturnType!F();
+		else
+			return callback(this.val);
+	}
+
+	mixin dispatch!(from_value, Maybe);
+	mixin monadicOperations!(Maybe);
+}
+
+unittest {
+	class C { }
+	class B {
+		C c;
+		this(C c) { this.c = c; }
+	}
+	class A {
+		B b;
+		this(B b) { this.b = b; }
+	}
+
+	C c = new C;
+	Maybe!A a1 = from_value!Maybe(new A(new B(c)));
+	Maybe!A a2 = from_value!Maybe(new A(null));
+
+	auto getB = delegate (A a) => new Maybe!B(a.b);
+	auto getC = (B b) => new Maybe!C(b.c);
+
+	assert (a1.b.c.value is c);
+	assert (a1.bind(getB).bind(getC).value is c);
+	assert ((a1 >> getB >> getC).value is c);
+	assert (a2.b.c.value is null);
+}
